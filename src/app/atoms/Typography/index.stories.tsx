f5b1afed96fd027898fe83ade47e0795f32bc2fc
jiin <jiin.kim109@gmail.com>
2024-01-30 22:39:53 -0800

    update storybook

--- src/app/atoms/Typography/index.stories.tsx
+++ src/app/atoms/Typography/index.stories.tsx
@@ -1,17 +1,20 @@
+import { View } from 'react-native';
 import { ComponentMeta, StoryFn, StoryObj } from '@storybook/react-native';
 import Typography from './index';
-import { TypographyProps} from './types';
-import { View } from 'react-native';
+import { TypographyProps } from './types';
 
 const meta: ComponentMeta<typeof Typography> = {
     title: 'Design System/Atoms/Typography',
     component: Typography,
+    render: args => (
+        <Typography {...args}>Lorem ipsum dolor sit amet</Typography>
+    ),
     argTypes: {
         variant: {
             options: ['title', 'subtitle', 'text', 'subtext'],
             control: { type: 'radio' },
         },
-            color: {
+        color: {
             options: ['text', 'subtext', 'shade', 'notification'],
             control: { type: 'radio' },
         },
@@ -41,6 +44,5 @@ export const Title: Story = {
     args: {
         variant: 'title',
         color: 'text',
-        children: 'Example',
     },
 };
