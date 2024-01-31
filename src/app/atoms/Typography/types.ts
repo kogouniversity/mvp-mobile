8062f2e0f030466d3c12139b44f060295c01c7ab
scott0929 <scott0929@gmail.com>
2024-01-30 21:08:51 -0800

    typography

--- src/app/atoms/Typography/types.ts
+++ src/app/atoms/Typography/types.ts
@@ -2,19 +2,21 @@ import { TextStyle } from 'react-native';
 import { TextProps } from 'react-native-svg';
 
 export type TypographyVariants =
-    | 'h1'
-    | 'h2'
-    | 'h3'
-    | 'h4'
-    | 'h5'
-    | 'h6'
-    | 'subtitle1'
-    | 'subtitle2'
-    | 'body1'
-    | 'body2'
-    | 'button';
+    | 'title'
+    | 'subtitle'
+    | 'text'
+    | 'subtext'
+;
+
+export type TypographyColors =
+    | 'text'
+    | 'subtext'
+    | 'shade'
+    | 'notification'
+;
 
 export type TypographyProps = {
     variant: TypographyVariants;
+    color: TypographyColors;
     style?: TextStyle;
 } & TextProps;
