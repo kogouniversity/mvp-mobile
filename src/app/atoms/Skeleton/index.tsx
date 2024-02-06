e23d6cb57c5176a2ca5bb6b6e8250aed7ef11d7d
Sanghyun Park <spa202@sfu.ca>
2024-02-06 02:43:19 -0800

    skeleton atom

---
+++ src/app/atoms/Skeleton/index.tsx
@@ -0,0 +1,63 @@
+import React, { useRef, useEffect } from 'react';
+import { View, Animated, ViewStyle } from 'react-native';
+import { SkeletonProps, SkeletonVariant } from './types';
+
+const skeletonVariantStyles: Record<SkeletonVariant, ViewStyle> = {
+  text: {
+
+  },
+  circular: {
+    borderRadius: 9999,
+  },
+  rectangular: {
+    
+  },
+  rounded: {
+    borderRadius: 10,
+  },
+};
+
+const Skeleton: React.FC<SkeletonProps> = ({
+  variant = 'text',
+  width,
+  height,
+  style = {},
+}) => {
+  const opacity = useRef(new Animated.Value(0.3)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(opacity, {
+          toValue: 1,
+          useNativeDriver: true,
+          duration: 500,
+        }),
+        Animated.timing(opacity, {
+          toValue: 0.3,
+          useNativeDriver: true,
+          duration: 800,
+        }),
+      ])
+    ).start();
+  }, [opacity]);
+
+  const baseStyle: ViewStyle = {
+    width,
+    height,
+    backgroundColor: '#e0e0e0',
+  };
+
+  return (
+    <Animated.View
+      style={[
+        baseStyle,
+        skeletonVariantStyles[variant],
+        style,
+        { opacity }, 
+      ]}
+    />
+  );
+};
+
+export default Skeleton;
---
+++ src/app/atoms/Skeleton/types.ts
@@ -0,0 +1,10 @@
+import { ViewStyle } from 'react-native';
+
+export type SkeletonVariant = 'text' | 'circular' | 'rectangular' | 'rounded';
+
+export interface SkeletonProps {
+  variant?: SkeletonVariant;
+  width: number;
+  height: number;
+  style?: ViewStyle;
+}
