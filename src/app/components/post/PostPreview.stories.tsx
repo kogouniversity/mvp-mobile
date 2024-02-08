4b84d7568b2f0c6898479386fe6e93cb65a34fe3
scott0929 <scott0929@gmail.com>
2024-02-08 12:15:52 -0800

    ppstory

--- src/app/components/post/PostPreview.stories.tsx
+++ src/app/components/post/PostPreview.stories.tsx
@@ -4,6 +4,8 @@ import { PostPreviewProps } from './types';
 import { View } from 'react-native';
 import type { ComponentMeta, StoryFn, StoryObj } from '@storybook/react-native';
 import { ImageSourcePropType } from 'react-native';
+import { List, ListItem, ListItemButton } from '../../atoms/List/index';
+import { StyleSheet } from 'react-native';
 
 const meta: ComponentMeta<typeof PostPreview> = {
     title: 'Design System/components/post/PostPreview',
@@ -24,9 +26,20 @@ const meta: ComponentMeta<typeof PostPreview> = {
 
 export default meta;
 
-type Story = StoryObj<PostPreviewProps>;
+type PPStory = StoryObj<PostPreviewProps>;
+type PPLStory = StoryObj<PostPreviewProps[]>;
 
-export const Default: Story = {
+const styles = StyleSheet.create({
+    verticalList: {
+        borderBottomWidth: 0.5,
+        borderColor: '#d3d3d3',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+});
+
+export const Default: PPStory = {
     args: {
         width: 390,
         height: 74,
@@ -40,4 +53,51 @@ export const Default: Story = {
         authorSchoolName: "UBC",
         authorName: "unknown123",
     },
+};
+
+export const ListDefault: PPLStory = {
+    args: [
+        {
+            width: 390,
+            height: 74,
+            imageLink: require('../../assets/images/chick.png') as ImageSourcePropType,
+            groupName: "벤쿠버 유학생 방",
+            title: "오늘 저녁 뭐 먹을지 고르는거 도와주셈",
+            contentPreview: "모짜솔솔김볶밥 vs 차돌돌돌만불닭볶밥",
+            timestamp: new Date(),
+            numOfLikes: 13,
+            numOfComments: 5,
+            authorSchoolName: "UBC",
+            authorName: "unknown123",
+            onPress: () => console.log("pressed"),
+        },
+        {
+            width: 390,
+            height: 74,
+            imageLink: require('../../assets/images/chick.png') as ImageSourcePropType,
+            groupName: "벤쿠버 유학생 방",
+            title: "오늘 저녁 뭐 먹을지 고르는거 도와주셈",
+            contentPreview: "모짜솔솔김볶밥 vs 차돌돌돌만불닭볶밥",
+            timestamp: new Date(),
+            numOfLikes: 300,
+            numOfComments: 5,
+            authorSchoolName: "UBC",
+            authorName: "unknown123",
+            onPress: () => console.log("pressed"),
+        },
+    ],
+    render: args => (
+        <List {...args}>
+            <ListItem style={styles.verticalList}>
+                <ListItemButton>
+                    <PostPreview {...args[0]} />
+                </ListItemButton>
+            </ListItem>
+            <ListItem style={styles.verticalList}>
+                <ListItemButton>
+                    <PostPreview {...args[0]} />
+                </ListItemButton>
+            </ListItem>
+        </List>
+    ),
 };
