be9fefc42397fdeeff0b8c9cdc574551ea6816cf
jiin <jiin.kim109@gmail.com>
2024-02-13 21:13:48 -0800

    add setupTest for integration test

--- src/test-automation/integration/login.integration.test.tsx
+++
@@ -1,9 +0,0 @@
-describe('Login Integration Test', () => {
-    describe('When your pressed submit without ID and/or Password', () => {
-        it('should not navigate to the next page and show an error message', () => {});
-    });
-    describe('When your entered ID and Password', () => {
-        it('should not navigate to the next page if user does not exist', () => {});
-        it('should navigate to the next page if user exists', () => {});
-    });
-});
