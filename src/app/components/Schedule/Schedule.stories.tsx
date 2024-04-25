3d069446632faeadd1ea7de2c0fff5447071fb68
Sanghyun Park <spa202@sfu.ca>
2024-04-25 01:02:10 -0700

    schedule component

---
+++ src/app/components/Schedule/Schedule.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentMeta, StoryFn, StoryObj } from '@storybook/react-native';
+import Schedule from './Schedule';
+import courses from './courses.json'
+
+export default {
+    title: 'Components/Schedule',
+    component: Schedule,
+} as ComponentMeta<typeof Schedule>;
+
+const Template: StoryFn<typeof Schedule> = (args) => <Schedule courses={courses} {...args} />;
+
+export const Default: StoryObj<typeof Schedule> = {
+    render: Template,
+    args: {
+    
+    }
+}
